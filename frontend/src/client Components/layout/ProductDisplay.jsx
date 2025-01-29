375a7ab14bebc2dca322894c9bfe9ddd203d61fd
Sivabalan <sivabalanm205@gmail.com>
2025-01-29 11:20:27 +0530

    mt-50px

--- frontend/src/client Components/layout/ProductDisplay.jsx
+++ frontend/src/client Components/layout/ProductDisplay.jsx
@@ -75,7 +75,7 @@ function ProductDisplay() {
         className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 mt-[50px]">
           <ul className="space-y-2 font-medium mt-10">
            
 
