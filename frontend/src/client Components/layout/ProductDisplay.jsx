b56d5c9d644b643ebdd07a1d1556a902f603fe5b
SnehaS-14 <sneha014csec@gmail.com>
2025-02-12 13:14:38 +0530

    product display pagecolor changes in button an rs symbol change

--- frontend/src/client Components/layout/ProductDisplay.jsx
+++ frontend/src/client Components/layout/ProductDisplay.jsx
@@ -159,7 +159,7 @@ function ProductDisplay() {
                         {product.title}
                       </h5>
                       <h6 className="text-base text-gray-800 font-bold ml-auto">
-                        ${product.price}
+                      ₹{product.price}
                       </h6>
                     </div>
                     <p className="text-gray-500 text-[13px] mt-2 truncate">
@@ -167,25 +167,10 @@ function ProductDisplay() {
                       {product.description}
                     </p>
                     <div className="flex items-center gap-2 mt-4">
-                      <div
-                        className="bg-pink-100 hover:bg-pink-200 w-12 h-9 flex items-center justify-center rounded cursor-pointer"
-                        title="Wishlist"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16px"
-                          className="fill-pink-600 inline-block"
-                          viewBox="0 0 64 64"
-                        >
-                          <path
-                            d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                            data-original="#000000"
-                          />
-                        </svg>
-                      </div>
+                   
                       <button
                         type="button"
-                        className="text-sm px-2 h-9 font-semibold w-full bg-blue-600 hover:bg-blue-700 text-white tracking-wide ml-auto outline-none border-none rounded"
+                        className="text-sm px-2 h-9 font-semibold w-full bg-green-600 hover:bg-green-700 text-white tracking-wide ml-auto outline-none border-none rounded"
                       >
                       <Link to={`/products/${product._id}`}>
                         {product.available}
