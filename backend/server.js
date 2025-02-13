6afb8eda82e5a8f0efe8988d97da34d0f1a001e9
Sivabalan <sivabalanm205@gmail.com>
2025-02-13 13:14:42 +0530

    backend db

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,6 @@ app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded data
 // Database Connection
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB', err));
